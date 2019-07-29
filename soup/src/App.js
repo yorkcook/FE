9afd65b08bcbedce3bfd2135241a6cd61b2ad214
York Cook <yorkcook@Johnny-Castle.lan1>
2019-07-29 15:06:17 -0400

    WIP: barebones registration form built out

--- soup/src/App.js
+++ soup/src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 
+import Registration from "./components-york/Registration";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>I'll Serve Soup!</h1>
+      <Registration />
     </div>
   );
 }
